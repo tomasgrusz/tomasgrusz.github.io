40455deb34d5195982b5d833ae6db0e94a0f8f87
Tomas Grusz <grusztomas0@gmail.com>
2022-03-28 20:04:46 +0200

    Add h2, p to welcome

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,7 +1,31 @@
 import "./Welcome.css";
 
 function Welcome() {
-  return <p>Welcome!</p>;
+  return (
+    <section className="welcomeSection">
+      <h2>Welcome to my page!</h2>
+      <p>
+        My name is Tomas, and I am an international student from Slovakia
+        studying B.Sc. Computer Science at University of Birmingham. The first
+        time I came in contact with computer technology was over 15 years ago,
+        playing my brother’s video game. I was eager to explore all the
+        mysteries behind it. Since then, computer science and programming have
+        been my passion, inspiring me to delve into everything the field has to
+        offer. Among others, I am known as a kind and determined person. I like
+        being a part of teams and joining forces to get over obstacles
+        efficiently. Passionate about my studies, I also have no problem
+        spending countless hours studying and practising on my own. In my spare
+        time, I deepen my knowledge through various online courses, tutorials,
+        and community projects. Moreover, I love working on personal
+        mini-projects, so that I can practise what I have learnt, gain more
+        experience in problem-solving, not to mention challenge my creativity.
+        My dream is to create inspiring and entertaining applications, as well
+        as help others solve problems. I am aspiring to build a diverse
+        portfolio of projects and become a part of a great community of
+        colleagues.
+      </p>
+    </section>
+  );
 }
 
 export default Welcome;
